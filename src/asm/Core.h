bc9b5aa04bddcb00e44edc81f2b73d72d5d97b9e
Meonardo <sisxme@gmail.com>
2023-10-08 20:31:38 +0800

    Init commit.

---
+++ src/asm/Core.h
@@ -0,0 +1,22 @@
+
+#include <windows.h>
+#include <Macros.h>
+
+UINT_PTR GetRIP( VOID );
+
+typedef struct {
+
+    struct {
+        WIN32_FUNC( system );
+        WIN32_FUNC( LoadLibraryA );
+    } Win32; 
+
+    struct {
+        // Basics
+        HMODULE     Kernel32;
+        HMODULE     Ntdll;
+
+        HMODULE     MSVCRT;
+    } Modules;
+
+} INSTANCE, *PINSTANCE;
